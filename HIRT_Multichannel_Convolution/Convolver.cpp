d1fdaabf85fecc05482f8ed9552b482ddb6eaff9
Alex Harker <ajharker@gmail.com>
2019-03-01 11:00:31 +0000

    Simplify to align with other updates and use RAII when possible

--- HIRT_Multichannel_Convolution/Convolver.cpp
+++ HIRT_Multichannel_Convolution/Convolver.cpp
@@ -5,57 +5,48 @@
 #include <stdio.h>
 #include <string.h>
 
-HISSTools::Convolver::Convolver(uint32_t numIns, uint32_t numOuts,
-                                     LatencyMode latency)
+HISSTools::Convolver::Convolver(uint32_t numIns, uint32_t numOuts, LatencyMode latency)
+: mTemporaryMemory(0, 0)
 {
     numIns = numIns < 1 ? 1 : numIns;
-
+    
     mN2M = true;
     mNumIns = numIns;
     mNumOuts = numOuts;
-
+    
     for (uint32_t i = 0; i < numOuts; i++)
     {
-        mConvolvers.push_back(new HISSTools::NToMonoConvolve(
-            numIns, 16384, (t_convolve_latency_mode)latency));
-        mInTemps.push_back(NULL);
+        mConvolvers.push_back(new NToMonoConvolve(numIns, 16384, latency));
+        mInTemps.push_back(nullptr);
     }
-
-    mTemp1 = NULL;
-    mTemp2 = NULL;
-
-    alloc_memory_swap(&mTemporaryMemory, (AH_UIntPtr)0, (AH_UIntPtr)0);
+    
+    mTemp1 = nullptr;
+    mTemp2 = nullptr;
 }
 
 HISSTools::Convolver::Convolver(uint32_t numIO, LatencyMode latency)
+: mTemporaryMemory(0, 0)
 {
     numIO = numIO < 1 ? 1 : numIO;
-
+    
     mN2M = false;
     mNumIns = numIO;
     mNumOuts = numIO;
-
+    
     for (uint32_t i = 0; i < numIO; i++)
     {
-        mConvolvers.push_back(
-            new NToMonoConvolve(1, 16384, (t_convolve_latency_mode)latency));
-        mInTemps.push_back(NULL);
+        mConvolvers.push_back(new NToMonoConvolve(1, 16384, latency));
+        mInTemps.push_back(nullptr);
     }
-
-    mTemp1 = NULL;
-    mTemp2 = NULL;
-
-    alloc_memory_swap(&mTemporaryMemory, (AH_UIntPtr)0, (AH_UIntPtr)0);
+    
+    mTemp1 = nullptr;
+    mTemp2 = nullptr;
 }
 
 HISSTools::Convolver::~Convolver() throw()
 {
-    for (uint32_t i = 0; i < mNumOuts; i++) delete mConvolvers[i];
-
-    // FIX - which is correct?
-
-    free_memory_swap(&mTemporaryMemory);
-    // ALIGNED_FREE(mInTemps[0]);
+    for (uint32_t i = 0; i < mNumOuts; i++)
+        delete mConvolvers[i];
 }
 
 // Clear IRs
@@ -65,17 +56,19 @@ void HISSTools::Convolver::clear(bool resize)
     if (mN2M)
     {
         for (uint32_t i = 0; i < mNumOuts; i++)
-            for (uint32_t j = 0; j < mNumIns; j++) clear(j, i, resize);
+            for (uint32_t j = 0; j < mNumIns; j++)
+                clear(j, i, resize);
     }
     else
     {
-        for (uint32_t i = 0; i < mNumOuts; i++) clear(i, i, resize);
+        for (uint32_t i = 0; i < mNumOuts; i++)
+            clear(i, i, resize);
     }
 }
 
 void HISSTools::Convolver::clear(uint32_t inChan, uint32_t outChan, bool resize)
 {
-    set(inChan, outChan, (float*)NULL, 0, resize);
+    set(inChan, outChan, (float *)nullptr, 0, resize);
 }
 
 // DSP Engine Reset
@@ -96,15 +89,14 @@ void HISSTools::Convolver::reset()
 t_convolve_error HISSTools::Convolver::reset(uint32_t in_chan, uint32_t out_chan)
 {
     // For Parallel operation you must pass the same in/out channel
-
+    
     if (!mN2M) in_chan -= out_chan;
-
+    
     if (out_chan < mNumOuts)
         return mConvolvers[out_chan]->reset(in_chan);
     else
         return CONVOLVE_ERR_OUT_CHAN_OUT_OF_RANGE;
 }
-
 
 // Resize and set IR
 
@@ -113,9 +105,9 @@ t_convolve_error HISSTools::Convolver::resize(uint32_t inChan, uint32_t outChan,
                                                    uintptr_t length)
 {
     // For Parallel operation you must pass the same in/out channel
-
+    
     if (!mN2M) inChan -= outChan;
-
+    
     if (outChan < mNumOuts)
         return mConvolvers[outChan]->resize(inChan, length);
     else
@@ -127,9 +119,9 @@ t_convolve_error HISSTools::Convolver::set(uint32_t inChan, uint32_t outChan,
                                                 bool resize)
 {
     // For Parallel operation you must pass the same in/out channel
-
+    
     if (!mN2M) inChan -= outChan;
-
+    
     if (outChan < mNumOuts)
         return mConvolvers[outChan]->set(inChan, input, length, resize);
     else
@@ -141,14 +133,14 @@ t_convolve_error HISSTools::Convolver::set(uint32_t inChan, uint32_t outChan,
                                                 uintptr_t impulseLength, bool resize)
 {
     // For Parallel operation you must pass the same in/out channel
-
+    
     if (!mN2M)
         inChan -= outChan;
-
+    
     //t_convolve_error err
     //    = set(inChan, outChan, std::vector<float>(input, input + impulseLength).data(),
     //          impulseLength, resize);
-
+    
     
     float *inputFloat = new float[impulseLength];
     
@@ -158,77 +150,67 @@ t_convolve_error HISSTools::Convolver::set(uint32_t inChan, uint32_t outChan,
     t_convolve_error err = set(inChan, outChan, inputFloat, impulseLength, resize);
     
     delete[] inputFloat;
-
-    
     
     return err;
 }
+
 // DSP
 
-void HISSTools::Convolver::process(const double** ins,
-                                        double** outs, size_t numIns,
-                                        size_t numOuts, size_t numSamples)
+void HISSTools::Convolver::process(const float** ins, float** outs, size_t numIns, size_t numOuts, size_t numSamples)
 {
-    void* memPointer = grow_memory_swap(
-        &mTemporaryMemory, (mNumIns + 2) * numSamples * sizeof(float), numSamples);
-    tempSetup(memPointer, mTemporaryMemory.current_size);
-
-    if (!mTemporaryMemory.current_ptr)
+    auto memPointer = mTemporaryMemory.grow((mNumIns + 2) * numSamples * sizeof(float), numSamples);
+    tempSetup(memPointer.get(), memPointer.getSize());
+    
+    if (!memPointer.get())
         numIns = numOuts = 0;
+    
+    SIMDSettings settings;
+    
+    // FIX - nasty!
+    
+    for (size_t i = 0; i < numOuts; i++)
+        mConvolvers[i]->process(mN2M ? ins : ins + i, outs[i], mTemp1, numSamples, numIns);
+}
 
+void HISSTools::Convolver::process(const double** ins, double** outs, size_t numIns, size_t numOuts, size_t numSamples)
+{
+    auto memPointer = mTemporaryMemory.grow((mNumIns + 2) * numSamples * sizeof(float), numSamples);
+    tempSetup(memPointer.get(), memPointer.getSize());
+    
+    if (!memPointer.get())
+        numIns = numOuts = 0;
+    
     numIns = numIns > mNumIns ? mNumIns : numIns;
     numOuts = numOuts > mNumOuts ? mNumOuts : numOuts;
-
+    
     for (uintptr_t i = 0; i < numIns; i++)
         for (uintptr_t j = 0; j < numSamples; j++)
             mInTemps[i][j] = (float)ins[i][j];
     
-    setMXCSR();
+    SIMDSettings settings;
     
     // FIX - nasty!
     
     for (uintptr_t i = 0; i < numOuts; i++)
-        mConvolvers[i]->process((const float **)(mN2M ? &mInTemps[0] : (&mInTemps[0] + i)), outs[i], mTemp1, mTemp2, numSamples, numIns);
-
-    resetMXCSR();
-
-    unlock_memory_swap(&mTemporaryMemory);
+    {
+        mConvolvers[i]->process((const float **)(mN2M ? &mInTemps[0] : (&mInTemps[0] + i)), mTemp2, mTemp1, numSamples, numIns);
+        
+        for (uintptr_t j = 0; j < numSamples; j++)
+            outs[i][j] = mTemp2[j];
+    }
 }
 
-void HISSTools::Convolver::process(const float** ins,
-                                   float** outs, size_t numIns,
-                                   size_t numOuts, size_t numSamples)
-{
-    void* memPointer = grow_memory_swap(
-                                        &mTemporaryMemory, (mNumIns + 2) * numSamples * sizeof(float), numSamples);
-    tempSetup(memPointer, mTemporaryMemory.current_size);
-    
-    if (!mTemporaryMemory.current_ptr)
-        numIns = numOuts = 0;
-    
-    setMXCSR();
-    
-    // FIX - nasty!
-    
-    for (AH_UIntPtr i = 0; i < numOuts; i++)
-        mConvolvers[i]->process(mN2M ? ins : ins + i, outs[i], mTemp1, numSamples, numIns);
-    
-    resetMXCSR();
-    
-    unlock_memory_swap(&mTemporaryMemory);
-}
-
-void HISSTools::Convolver::tempSetup(void* memPointer, AH_UIntPtr maxFrameSize)
+void HISSTools::Convolver::tempSetup(void* memPointer, uintptr_t maxFrameSize)
 {
     mInTemps[0] = (float*)memPointer;
-
+    
     for (uint32_t i = 1; i < mNumIns; i++) mInTemps[i] = mInTemps[0] + (i * maxFrameSize);
-
+    
     mTemp1 = mInTemps[mNumIns - 1] + maxFrameSize;
     mTemp2 = mTemp1 + maxFrameSize;
 }
 
-void HISSTools::Convolver::setMXCSR()
+HISSTools::Convolver::SIMDSettings::SIMDSettings()
 {
 #if defined(__i386__) || defined(__x86_64__)
     mOldMXCSR = _mm_getcsr();
@@ -236,7 +218,7 @@ void HISSTools::Convolver::setMXCSR()
 #endif
 }
 
-void HISSTools::Convolver::resetMXCSR()
+HISSTools::Convolver::SIMDSettings::~SIMDSettings()
 {
 #if defined(__i386__) || defined(__x86_64__)
     _mm_setcsr(mOldMXCSR);
--- HIRT_Multichannel_Convolution/Convolver.h
+++ HIRT_Multichannel_Convolution/Convolver.h
@@ -4,35 +4,36 @@
 #include <stdint.h>
 #include <vector>
 
-#include "AH_Generic_Memory_Swap.h"
+#include "MemorySwap.h"
 #include "NToMonoConvolve.h"
 #include "convolve_errors.h"
 
 namespace HISSTools
 {
-   class Convolver
+    class Convolver
     {
-
-    public:
-        enum LatencyMode
+        struct SIMDSettings
         {
-            kLatencyZero,
-            kLatencyShort,
-            kLatencyMedium
+            SIMDSettings();
+            ~SIMDSettings();
+            
+            unsigned int mOldMXCSR;
         };
-
+        
+    public:
+        
         Convolver(uint32_t numIns, uint32_t numOuts, LatencyMode latency);
         Convolver(uint32_t numIO, LatencyMode latency);
-
+        
         virtual ~Convolver() throw();
-
+        
         // Clear IRs
-
+        
         void clear(bool resize);
         void clear(uint32_t inChan, uint32_t outChan, bool resize);
-
+        
         // DSP Engine Reset
-
+        
         void reset();
         t_convolve_error reset(uint32_t in_chan, uint32_t out_chan);
 
@@ -47,36 +48,32 @@ namespace HISSTools
                              uintptr_t length, bool resize);
 
         // DSP
-
+        
         void process(const double** ins, double** outs, size_t numIns, size_t numOuts, size_t numSamples);
         
         // FIX - consts
         
         void process(const float** ins, float** outs, size_t numIns, size_t numOuts, size_t numSamples);
-       
+        
     private:
         
-        void tempSetup(void* memPointer, AH_UIntPtr maxFrameSize);
-
-        void setMXCSR();
-        void resetMXCSR();
-
+        void tempSetup(void* memPointer, uintptr_t maxFrameSize);
+        
         // Data
-
+        
         uint32_t mNumIns;
         uint32_t mNumOuts;
         bool mN2M;
-
+        
         std::vector<float*> mInTemps;
         float* mTemp1;
         float* mTemp2;
-
-        t_memory_swap mTemporaryMemory;
-
+        
+        MemorySwap<float> mTemporaryMemory;
+        
         std::vector<NToMonoConvolve*> mConvolvers;
-
-        unsigned int mOldMXCSR;
     };
 }
 
 #endif
+
