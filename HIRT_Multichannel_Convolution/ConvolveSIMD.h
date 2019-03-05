fe8cdad975831f250ec4f457b37e5e9b97e7f767
Alex Harker <ajharker@gmail.com>
2019-03-05 09:31:37 +0000

    Added ARM support for SIMD convolution

--- HIRT_Multichannel_Convolution/ConvolveSIMD.h
+++ HIRT_Multichannel_Convolution/ConvolveSIMD.h
@@ -1,6 +1,59 @@
 
 #pragma once
 
+template <class T, class U, int vec_size> struct SIMDVector
+{
+    static const int size = vec_size;
+    typedef T scalar_type;
+    
+    SIMDVector() {}
+    SIMDVector(U a) : mVal(a) {}
+    
+    U mVal;
+};
+
+#if defined (__arm__)
+
+#include <arm_neon.h>
+
+#define ALIGNED_MALLOC(x) aligned_alloc(16, x);
+#define ALIGNED_FREE free
+
+struct ARMFloat : public SIMDVector<float, float32x4_t, 4>
+{
+    ARMFloat() {}
+    ARMFloat(uint32x4_t a) : SIMDVector(a) {}
+    ARMFloat(float a) : SIMDVector(vdupq_n_f32(a)) {}
+    
+    friend ARMFloat operator + (const ARMFloat& a, const ARMFloat& b) { return vaddq_f32(a.mVal, b.mVal); }
+    friend ARMFloat operator - (const ARMFloat& a, const ARMFloat& b) { return vsubq_f32(a.mVal, b.mVal); }
+    friend ARMFloat operator * (const ARMFloat& a, const ARMFloat& b) { return vmulq_f32(a.mVal, b.mVal); }
+    
+    ARMFloat operator += (const ARMFloat& a)
+    {
+        *this = *this + a;
+        return *this;
+    }
+    
+    static ARMFloat unaligned_load(const float* ptr) { return vld1q_f32(ptr); }
+    
+    void unaligned_store(float* ptr)
+    {
+        vst1q_f32(ptr, mVal);
+    }
+    
+    float sum()
+    {
+        float values[4];
+        unaligned_store(values);
+        return values[0] + values[1] + values[2] + values[3];
+    }
+};
+
+typedef ARMFloat FloatVector;
+
+#else
+
 #ifdef __APPLE__
 #include <Accelerate/Accelerate.h>
 #define ALIGNED_MALLOC malloc
@@ -12,17 +65,6 @@
 #define ALIGNED_MALLOC(x)  _aligned_malloc(x, 16)
 #define ALIGNED_FREE(x)  _aligned_free(x)
 #endif
-
-template <class T, class U, int vec_size> struct SIMDVector
-{
-    static const int size = vec_size;
-    typedef T scalar_type;
-    
-    SIMDVector() {}
-    SIMDVector(U a) : mVal(a) {}
-    
-    U mVal;
-};
 
 struct SSEFloat : public SIMDVector<float, __m128, 4>
 {
@@ -57,3 +99,4 @@ struct SSEFloat : public SIMDVector<float, __m128, 4>
 
 typedef SSEFloat FloatVector;
 
+#endif
