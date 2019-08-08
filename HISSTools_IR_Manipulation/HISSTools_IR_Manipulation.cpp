31df84ae7a9b56fb4c87a5d7ce3bdd2b62750514
Alex Harker <ajharker@gmail.com>
2019-08-08 01:49:12 +0100

    Refactor to include float code paths (expect for for phase for now)

--- HISSTools_IR_Manipulation/HISSTools_IR_Manipulation.cpp
+++ HISSTools_IR_Manipulation/HISSTools_IR_Manipulation.cpp
@@ -57,88 +57,169 @@ void ir_real_operation(T *out, const T *in, uintptr_t fft_size, Op op)
         op(r_out + i, i_out + i, r_in[i], i_in[i], i);
 }
 
+// Functors
+
+template <typename T>
+struct copy
+{
+    void operator()(T *r_out, T *i_out, T r_in, T i_in, uintptr_t i)
+    {
+        *r_out = r_in;
+        *i_out = i_in;
+    }
+};
+
+template <typename T>
+struct amplitude
+{
+    void operator()(T *r_out, T *i_out, T r_in, T i_in, uintptr_t i)
+    {
+        *r_out = std::sqrt(r_in * r_in + i_in * i_in);
+        *i_out = 0.0;
+    }
+};
+
+template <typename T>
+struct conjugate
+{
+    void operator()(T *r_out, T *i_out, T r_in, T i_in, uintptr_t i)
+    {
+        *r_out =  r_in;
+        *i_out = -i_in;
+    }
+};
+
+struct log_power
+{
+    void operator()(double *r_out, double *i_out, double r_in, double i_in, uintptr_t i)
+    {
+        static double min_power = std::pow(10.0, -300.0 / 10.0);
+        double power = r_in * r_in + i_in * i_in;
+        
+        // FIX - scaling - 0.25 is for the real FFT effect
+        *r_out = 0.5 * log(0.25 * std::max(power, min_power));
+        *i_out = 0.0;
+    }
+};
+
+struct complex_exponential
+{
+    void operator()(double *r_out, double *i_out, double r_in, double i_in, uintptr_t i)
+    {
+        using complex = std::complex<double>;
+        const complex c = std::exp(complex(r_in, i_in));
+        
+        *r_out = std::real(c);
+        *i_out = std::imag(c);
+    }
+};
+
+struct phase_interpolate
+{
+    
+    // N.B. - induce a delay of -1 sample for anything over linear end avoid wraparound end the first sample
+    
+    phase_interpolate(double phase, double fft_size, bool zero_center)
+    {
+        phase = std::max(0.0, std::min(1.0, phase));
+        min_factor = 1.0 - (2.0 * phase);
+        lin_factor = zero_center ? 0.0 : (phase <= 0.5 ? -(2 * M_PI * phase) : (-2.0 * M_PI * (phase - 1.0 / fft_size)));
+    }
+    
+    void operator()(double *r_out, double *i_out, double r_in, double i_in, uintptr_t i)
+    {
+        const double amp = std::exp(r_in);
+        const double interp_phase = lin_factor * i + min_factor * i_in;
+        
+        r_out[i] = amp * std::cos(interp_phase);
+        i_out[i] = amp * std::sin(interp_phase);
+    }
+    
+    double min_factor;
+    double lin_factor;
+};
+
+template <typename T>
+struct spike
+{
+    spike(double spike_position, double fft_size)
+    {
+        spike_constant = ((long double) (2.0 * M_PI)) *  -spike_position / fft_size;
+    }
+    
+    void operator()(T *r_out, T *i_out, uintptr_t i)
+    {
+        long double phase = spike_constant * i;
+        *r_out = static_cast<T>(std::cos(phase));
+        *i_out = static_cast<T>(std::sin(phase));
+    }
+    
+    long double spike_constant;
+};
+
+template <typename T>
+struct delay_calc : private spike<T>
+{
+    delay_calc(double delay, double fft_size) : spike<T>(delay, fft_size) {}
+    
+    void operator()(T *r_out, T *i_out, T r_in, T i_in, uintptr_t i)
+    {
+        using complex = std::complex<T>;
+        
+        long double phase = spike<T>::spike_constant * i;
+        complex c = complex(r_in, i_in) * complex(std::cos(phase), std::sin(phase));
+        
+        r_out[i] = std::real(c);
+        i_out[i] = std::imag(c);
+    }
+};
+
+// Functions
+
+void ir_copy(FFT_SPLIT_COMPLEX_F *out, FFT_SPLIT_COMPLEX_F *in, uintptr_t fft_size)
+{
+    ir_real_operation(out, in, fft_size, copy<float>());
+}
+
 void ir_copy(FFT_SPLIT_COMPLEX_D *out, FFT_SPLIT_COMPLEX_D *in, uintptr_t fft_size)
 {
-    struct copy
-    {
-        void operator()(double *r_out, double *i_out, double r_in, double i_in, uintptr_t i)
-        {
-            *r_out = r_in;
-            *i_out = i_in;
-        }
-    };
-    
-    ir_real_operation(out, in, fft_size, copy());
+    ir_real_operation(out, in, fft_size, copy<double>());
+}
+
+void ir_spike(FFT_SPLIT_COMPLEX_F *out, uintptr_t fft_size, double spike_position)
+{
+    ir_real_operation(out, fft_size, spike<float>(spike_position, fft_size));
 }
 
 void ir_spike(FFT_SPLIT_COMPLEX_D *out, uintptr_t fft_size, double spike_position)
 {
-    struct spike
-    {
-        spike(double spike_position, double fft_size)
-        {
-            spike_constant = ((long double) (2.0 * M_PI)) * (fft_size - spike_position) / fft_size;
-        }
-        
-        void operator()(double *r_out, double *i_out, uintptr_t i)
-        {
-            long double phase = spike_constant * i;
-            *r_out = std::cos(phase);
-            *i_out = std::sin(phase);
-        }
-        
-        long double spike_constant;
-    };
-    
-    ir_real_operation(out, fft_size, spike(spike_position, fft_size));
+    ir_real_operation(out, fft_size, spike<double>(spike_position, fft_size));
+}
+
+void ir_delay(FFT_SPLIT_COMPLEX_F *out, FFT_SPLIT_COMPLEX_F *in, uintptr_t fft_size, double delay)
+{
+    if (delay != 0.0)
+        ir_real_operation(out, in, fft_size, delay_calc<float>(delay, fft_size));
+    else if (in != out)
+        ir_copy(out, in, fft_size);
 }
 
 void ir_delay(FFT_SPLIT_COMPLEX_D *out, FFT_SPLIT_COMPLEX_D *in, uintptr_t fft_size, double delay)
 {
-    struct delay_calc
-    {
-        delay_calc(double delay, double fft_size)
-        {
-            delay_constant = ((long double) (2.0 * M_PI)) * -delay / fft_size;
-        }
-        
-        void operator()(double *r_out, double *i_out, double r_in, double i_in, uintptr_t i)
-        {
-            using complex = std::complex<double>;
-            
-            long double phase = delay_constant * i;
-            complex c = complex(r_in, i_in) * complex(std::cos(phase), std::sin(phase));
-            
-            r_out[i] = std::real(c);
-            i_out[i] = std::imag(c);
-        }
-        
-        long double delay_constant;
-    };
-    
-    if (delay == 0.0)
-    {
-        if (in != out)
-            ir_copy(out, in, fft_size);
-        
-        return;
-    }
-    
-    ir_real_operation(out, in, fft_size, delay_calc(delay, fft_size));
+    if (delay != 0.0)
+        ir_real_operation(out, in, fft_size, delay_calc<double>(delay, fft_size));
+    else if (in != out)
+        ir_copy(out, in, fft_size);
+}
+
+void ir_time_reverse(FFT_SPLIT_COMPLEX_F *out, FFT_SPLIT_COMPLEX_F *in, uintptr_t fft_size)
+{
+    ir_real_operation(out, in, fft_size, conjugate<float>());
 }
 
 void ir_time_reverse(FFT_SPLIT_COMPLEX_D *out, FFT_SPLIT_COMPLEX_D *in, uintptr_t fft_size)
 {
-    struct conjugate
-    {
-        void operator()(double *r_out, double *i_out, double r_in, double i_in, uintptr_t i)
-        {
-            *r_out =  r_in;
-            *i_out = -i_in;
-        }
-    };
-    
-    ir_real_operation(out, in, fft_size, conjugate());
+    ir_real_operation(out, in, fft_size, conjugate<double>());
 }
 
 void minimum_phase_components(FFT_SETUP_D setup, FFT_SPLIT_COMPLEX_D *out, FFT_SPLIT_COMPLEX_D *in, uintptr_t fft_size)
@@ -148,19 +229,6 @@ void minimum_phase_components(FFT_SETUP_D setup, FFT_SPLIT_COMPLEX_D *out, FFT_S
     uintptr_t fft_size_log2;
     
     // Take Log of Power Spectrum
-    
-    struct log_power
-    {
-        void operator()(double *r_out, double *i_out, double r_in, double i_in, uintptr_t i)
-        {
-            static double min_power = std::pow(10.0, -300.0 / 10.0);
-            double power = r_in * r_in + i_in * i_in;
-            
-            // FIX - scaling - 0.25 is for the real FFT effect
-            *r_out = 0.5 * log(0.25 * std::max(power, min_power));
-            *i_out = 0.0;
-        }
-    };
     
     ir_real_operation(out, out, fft_size, log_power());
 
@@ -198,94 +266,32 @@ void minimum_phase_components(FFT_SETUP_D setup, FFT_SPLIT_COMPLEX_D *out, FFT_S
     hisstools_rfft(setup, out, fft_size_log2);
 }
 
-void convert_to_minimum_phase(FFT_SETUP_D setup, FFT_SPLIT_COMPLEX_D *out, FFT_SPLIT_COMPLEX_D *in, uintptr_t fft_size)
-{
-    minimum_phase_components(setup, out, in, fft_size);
-    
-    struct complex_exponential
-    {
-        void operator()(double *r_out, double *i_out, double r_in, double i_in, uintptr_t i)
-        {
-            using complex = std::complex<double>;
-            const complex c = std::exp(complex(r_in, i_in));
-            
-            *r_out = std::real(c);
-            *i_out = std::imag(c);
-        }
-    };
-    
-    ir_real_operation(out, out, fft_size, complex_exponential());
-}
-
-void convert_to_mixed_phase(FFT_SETUP_D setup, FFT_SPLIT_COMPLEX_D *out, FFT_SPLIT_COMPLEX_D *in, uintptr_t fft_size, double phase, bool zero_center)
-{
-    minimum_phase_components(setup, out, in, fft_size);
-
-    phase = phase < 0 ? 0.0 : phase;
-    phase = phase > 1 ? 1.0 : phase;
-    
-    struct phase_interpolate
-    {
-        
-        // N.B. - induce a delay of -1 sample for anything over linear end avoid wraparound end the first sample
-        
-        phase_interpolate(double phase, double fft_size, bool zero_center)
-        {
-            min_factor = 1.0 - (2.0 * phase);
-            lin_factor = zero_center ? 0.0 : (phase <= 0.5 ? -(2 * M_PI * phase) : (-2.0 * M_PI * (phase - 1.0 / fft_size)));
-        }
-        
-        void operator()(double *r_out, double *i_out, double r_in, double i_in, uintptr_t i)
-        {
-            const double amp = std::exp(r_in);
-            const double interp_phase = lin_factor * i + min_factor * i_in;
-            
-            r_out[i] = amp * std::cos(interp_phase);
-            i_out[i] = amp * std::sin(interp_phase);
-        }
-        
-        double min_factor;
-        double lin_factor;
-    };
-    
-    ir_real_operation(out, out, fft_size, phase_interpolate(phase, fft_size, zero_center));
-}
-
-void convert_to_zero_phase(FFT_SPLIT_COMPLEX_D *out, FFT_SPLIT_COMPLEX_D *in, uintptr_t fft_size)
-{
-    struct amplitude
-    {
-        void operator()(double *r_out, double *i_out, double r_in, double i_in, uintptr_t i)
-        {
-            *r_out = std::sqrt(r_in * r_in + i_in * i_in);
-            *i_out = 0.0;
-        }
-    };
-    
-    ir_real_operation(out, in, fft_size, amplitude());
-}
-
 void ir_phase(FFT_SETUP_D setup, FFT_SPLIT_COMPLEX_D *out, FFT_SPLIT_COMPLEX_D *in, uintptr_t fft_size, double phase, bool zero_center)
 {
-    if (phase == 0.0)
+    if (phase == 0.5)
     {
-        convert_to_minimum_phase(setup, out, in, fft_size);
-    }
-    else if (phase == 0.5)
-    {
-        convert_to_zero_phase(out, in, fft_size);
+        ir_real_operation(out, in, fft_size, amplitude<double>());
         if (!zero_center)
             ir_delay(out, out, fft_size, fft_size >> 1);
     }
-    else if (phase == 1.0)
-    {
-        convert_to_minimum_phase(setup, out, in, fft_size);
-        ir_time_reverse(out, out, fft_size);
-        if (!zero_center)
-            ir_delay(out, out, fft_size, -1.0);
-    }
     else
     {
-        convert_to_mixed_phase(setup, out, in, fft_size, phase, zero_center);
+        minimum_phase_components(setup, out, in, fft_size);
+
+        if (phase == 0.0 || phase == 1.0)
+        {
+            ir_real_operation(out, out, fft_size, complex_exponential());
+            
+            if (phase == 1.0)
+            {
+                ir_time_reverse(out, out, fft_size);
+                if (!zero_center)
+                    ir_delay(out, out, fft_size, -1.0);
+            }
+        }
+        else
+        {
+            ir_real_operation(out, out, fft_size, phase_interpolate(phase, fft_size, zero_center));
+        }
     }
 }
--- HISSTools_IR_Manipulation/HISSTools_IR_Manipulation.h
+++ HISSTools_IR_Manipulation/HISSTools_IR_Manipulation.h
@@ -4,12 +4,16 @@
 
 #include "../HISSTools_FFT/HISSTools_FFT.h"
 
+void ir_copy(FFT_SPLIT_COMPLEX_F *out, FFT_SPLIT_COMPLEX_F *in, uintptr_t fft_size);
 void ir_copy(FFT_SPLIT_COMPLEX_D *out, FFT_SPLIT_COMPLEX_D *in, uintptr_t fft_size);
 
+void ir_spike(FFT_SPLIT_COMPLEX_F *out, uintptr_t fft_size, double spike_position);
 void ir_spike(FFT_SPLIT_COMPLEX_D *out, uintptr_t fft_size, double spike_position);
 
+void ir_delay(FFT_SPLIT_COMPLEX_F *out, FFT_SPLIT_COMPLEX_F *in, uintptr_t fft_size, double delay);
 void ir_delay(FFT_SPLIT_COMPLEX_D *out, FFT_SPLIT_COMPLEX_D *in, uintptr_t fft_size, double delay);
 
+void ir_time_reverse(FFT_SPLIT_COMPLEX_F *out, FFT_SPLIT_COMPLEX_F *in, uintptr_t fft_size);
 void ir_time_reverse(FFT_SPLIT_COMPLEX_D *out, FFT_SPLIT_COMPLEX_D *in, uintptr_t fft_size);
 
 void ir_phase(FFT_SETUP_D setup, FFT_SPLIT_COMPLEX_D *out, FFT_SPLIT_COMPLEX_D *in, uintptr_t fft_size, double phase, bool zero_center = false);
