55286817dde60bda166e3c11b09fb24539714e9e
Alex Harker <ajharker@gmail.com>
2019-08-14 11:23:15 +0100

    Added the concept of an allocator and some basic allocators

---
+++ Allocator.hpp
@@ -0,0 +1,27 @@
+
+#ifndef ALLOCATOR_HPP
+#define ALLOCATOR_HPP
+
+#include <cstdlib>
+
+#include "SIMDSupport.hpp"
+
+namespace impl
+{
+    typedef void *(*allocate_function)(size_t);
+    typedef void (*free_function)(void *);
+};
+
+template<impl::allocate_function alloc, impl::free_function dealloc>
+struct function_allocator
+{
+    template <typename T>
+    T* allocate(size_t size) { return reinterpret_cast<T*>(alloc(size * sizeof(T))); }
+    
+    template <typename T>
+    void deallocate(T *ptr) { dealloc(ptr); }
+};
+
+using malloc_allocator = function_allocator<malloc, free>;
+
+#endif
