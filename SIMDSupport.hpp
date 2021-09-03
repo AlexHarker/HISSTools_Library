5dd853049502b42f4ced9a5feb3e0d7f1ddac63c
Alex Harker <ajharker@gmail.com>
2021-09-03 09:10:22 +0100

    Fix type in cast

--- SIMDSupport.hpp
+++ SIMDSupport.hpp
@@ -1022,7 +1022,7 @@ template <int N>
 SIMDType<float, N> abs(const SIMDType<float, N> a)
 {
     const static uint32_t bit_mask_32 = 0x7FFFFFFFU;
-    const float bit_mask_32f = *(reinterpret_cast<const double *>(&bit_mask_32));
+    const float bit_mask_32f = *(reinterpret_cast<const float *>(&bit_mask_32));
     
     return a & SIMDType<float, N>(bit_mask_32f);
 }
