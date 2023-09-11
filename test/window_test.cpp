708a1ac23a603f1e1b9c43a074524d21db17f887
lewardo <segfault.ninja@gmail.com>
2023-09-11 16:31:52 +0100

    `window_test.cpp` compiles and runs

--- test/window_test.cpp
+++ test/window_test.cpp
@@ -1,86 +1,15 @@
 
-#include <mach/mach.h>
-#include <mach/mach_time.h>
 #include <algorithm>
 #include <iostream>
-#include <sstream>
-#include <iomanip>
 #include <random>
+
+#include "test_utils/timer.hpp"
+#include "test_utils/tabbed_out.hpp"
 
 #include "../include/window_functions.hpp"
 #include "../include/audio_file/out_file.hpp"
 
-// Output
-
-void tabbedOut(const std::string& name, const std::string& text, int tab = 25)
-{
-    std::cout << std::setw(tab) << std::setfill(' ');
-    std::cout.setf(std::ios::left);
-    std::cout.unsetf(std::ios::right);
-    std::cout << name;
-    std::cout.unsetf(std::ios::left);
-    std::cout << text << "\n";
-}
-
-template <typename T>
-std::string to_string_with_precision(const T a_value, const int n = 4, bool fixed = true)
-{
-    std::ostringstream out;
-    if (fixed)
-        out << std::setprecision(n) << std::fixed << a_value;
-    else
-        out << std::setprecision(n) << a_value;
-    
-    return out.str();
-}
-
-// Timing
-
-class Timer
-{
-    
-public:
-    
-    Timer() : mStart(0), mStore1(0), mStore2(0) {}
-    
-    void start()
-    {
-        mStart = mach_absolute_time();
-    };
-    
-    void stop()
-    {
-        uint64_t end = mach_absolute_time();
-        
-        mach_timebase_info_data_t info;
-        mach_timebase_info(&info);
-        
-        uint64_t elapsed = ((end - mStart) * info.numer) / info.denom;
-        
-        mStore2 = mStore1;
-        mStore1 = elapsed;
-    }
-    
-    uint64_t finish(const std::string& msg)
-    {
-        tabbedOut(msg + " Elapsed ", to_string_with_precision(mStore1 / 1000000.0, 2), 35);
-        
-        uint64_t elapsed = mStore1;
-        
-        return elapsed;
-    };
-    
-    void relative(const std::string& msg)
-    {
-        tabbedOut(msg + " Comparison ", to_string_with_precision(((double) mStore2 / (double) mStore1), 2), 35);
-    }
-    
-private:
-    
-    uint64_t        mStart;
-    uint64_t        mStore1;
-    uint64_t        mStore2;
-};
+using namespace htl_test_utils;
 
 std::default_random_engine rand_engine;
 
@@ -95,8 +24,8 @@ bool check_symmetry()
     int begin = random_integer(0, size / 2 + 2);
     int end = random_integer(size / 2 - 2, size);
     
-    double window1[size];
-    double window2[size];
+    double* window1 = new double[size];
+    double* window2 = new double[size];
 
     using namespace htl;
     using namespace window_functions;
@@ -111,9 +40,17 @@ bool check_symmetry()
             double relative_error = exp(fabs(log(window1[i] / window2[i - begin])));
         
             if (relative_error > 1.000000000001 || isnan(relative_error))
+            {
+                delete[] window1;
+                delete[] window2;
+
                 return false;
+            }
         }
     }
+
+    delete[] window1;
+    delete[] window2;
     
     return true;
 }
@@ -173,7 +110,7 @@ int main(int argc, const char * argv[])
     for (int i = 0; i < iter; i++)
         sine(window, size, 0, size, params());
     
-    Timer timer;
+    steady_timer timer;
     
     timer.start();
     for (int i = 0; i < iter; i++)
